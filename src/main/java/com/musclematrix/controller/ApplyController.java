31fd3ec562c41450dc75f76e4c044b48923bbdee
soo982181 <soo982181@naver.com>
2024-07-06 19:40:51 +0900

    server done

--- src/main/java/com/musclematrix/controller/ApplyController.java
+++ src/main/java/com/musclematrix/controller/ApplyController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.musclematrix.DTO.TeacherDTO;
@@ -21,26 +22,25 @@ public class ApplyController extends BaseController{
 	// PT선생님
 	private final TeacherService teacherService;
 	
-	// PT선생님 약력
-	private final HistoryService historyService;
-	
 	// PT선생님 진행 프로그램
 	private final ProgramService programService;
 	
 	@GetMapping("/apply/apply")
-	public String viewApply() {
-		List<Teacher> tvds = new ArrayList<Teacher>();
+	public String viewApply(Model model) {
+		List<TeacherDTO> tvds = new ArrayList<TeacherDTO>();
 		List<Teacher> teachers = teacherService.findAll();
 		
 		for (Teacher teacher : teachers) {
 			TeacherDTO tvd = new TeacherDTO();
-			tvd.setTeacher__name(teacher.getTeacher__name());
+			tvd.setTeacher_name(teacher.getTeacher_name());
 			tvd.setTeacher_profile(teacher.getTeacher_profile());
 			tvd.setTeacher_KAKAOLINK(teacher.getTeacher_KAKAOLINK());
+			tvd.setPrograms(programService.findAllByTeacherId(teacher.getTeacher_id()));
 			
-			
+			tvds.add(tvd);
 		}
 		
+		model.addAttribute("tvds", tvds);
 		return "/apply/apply";
 	}
 }
