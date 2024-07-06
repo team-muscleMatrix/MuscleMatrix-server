c2c1a34d7b668dceb76131a9cd5f7970f82b171f
soo982181 <soo982181@naver.com>
2024-07-06 16:47:46 +0900

    240706 pull

--- src/main/java/com/musclematrix/service/EventService.java
+++ src/main/java/com/musclematrix/service/EventService.java
@@ -1,7 +1,10 @@
 package com.musclematrix.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.musclematrix.domain.Event;
 import com.musclematrix.repository.EventRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -9,6 +12,32 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class EventService {
-	
+
 	private final EventRepository eventRepository;
+	
+	// 월요일
+	public List<Event> getEventsByStatusOne() {
+		return eventRepository.findEventsByStatusOne();
+	}
+  
+	// 화요일
+	public List<Event> getEventsByStatusTwo() {
+		return eventRepository.findEventsByStatusTwo();
+	}
+  
+	// 수요일
+	public List<Event> getEventsByStatusThree() {
+		return eventRepository.findEventsByStatusThree();
+	}
+  
+	// 목요일
+	public List<Event> getEventsByStatusFour() {
+		return eventRepository.findEventsByStatusFour();
+	}
+  
+	// 금요일
+	public List<Event> getEventsByStatusFive() {
+		return eventRepository.findEventsByStatusFive();
+	}
+	
 }
